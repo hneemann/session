94300610244554813f839c9035a26acb1e3e35e3
hneemann <helmut.neemann@arcor.de>
2026-04-16 07:56:52 +0200

    added hashing of username

--- files.go
+++ files.go
@@ -11,12 +11,32 @@ import (
 )
 
 type FileSystemFactory interface {
+	// Create created a user file system. If create is true, the file system
+	// needs to be created and an error needs to be returned if the user exists.
+	// If create is false an error needs to be returned if the user does not exist.
 	Create(user string, create bool) (fileSys.FileSystem, error)
+	// RenameUser renames the user folder. If the user does not
+	// exist, an error needs to be returned
+	RenameUser(user string, newName string) error
+	// DoesUserExist returns true if the user exists.
 	DoesUserExist(user string) bool
+	// DeleteOldUsers deletes the data of all users which data is older than the given age.
+	// The age is determined by the newest file in the user folder.
+	// If an error occurs, it needs to be returned, otherwise nil.
 	DeleteOldUsers(maxAge time.Duration) error
 }
 
 type fss string
+
+func (f fss) RenameUser(user string, newName string) error {
+	oldDir := filepath.Join(string(f), user)
+	newDir := filepath.Join(string(f), newName)
+	err := os.Rename(oldDir, newDir)
+	if err != nil {
+		log.Println("rename failed:" + err.Error())
+	}
+	return err
+}
 
 func (f fss) Create(user string, create bool) (fileSys.FileSystem, error) {
 	dir := filepath.Join(string(f), user)
@@ -110,6 +130,16 @@ func NewFileSystemFactory(folder string) FileSystemFactory {
 
 type mfs map[string]fileSys.MemoryFileSystem
 
+func (m mfs) RenameUser(user string, newName string) error {
+	if data, ok := m[user]; ok {
+		delete(m, user)
+		m[newName] = data
+		return nil
+	} else {
+		return os.ErrNotExist
+	}
+}
+
 func (m mfs) Create(user string, create bool) (fileSys.FileSystem, error) {
 	if create {
 		if _, ok := m[user]; ok {
@@ -142,28 +172,100 @@ func NewMemoryFileSystemFactory() FileSystemFactory {
 	return make(mfs)
 }
 
+// HashFunc is used to hash the username. It is used to create a hash of the username,
+// which is then used as the folder name for the user data. This can be used to obfuscate the usernames on disk.
+// It needs to return a valid folder name.
+type HashFunc func(string) string
+
 type hashUser struct {
-	parent FileSystemFactory
+	parent   FileSystemFactory
+	hashFunc HashFunc
 }
 
 func NewHashUser(f FileSystemFactory) FileSystemFactory {
-	return hashUser{f}
+	return hashUser{parent: f, hashFunc: HashUserSha1}
 }
 
 func (h hashUser) Create(user string, create bool) (fileSys.FileSystem, error) {
-	return h.parent.Create(HashUser(user), create)
+	return h.parent.Create(h.hashFunc(user), create)
+}
+
+func (h hashUser) RenameUser(user string, newName string) error {
+	return h.parent.RenameUser(h.hashFunc(user), h.hashFunc(newName))
 }
 
 func (h hashUser) DoesUserExist(user string) bool {
-	return h.parent.DoesUserExist(HashUser(user))
+	return h.parent.DoesUserExist(h.hashFunc(user))
 }
 
 func (h hashUser) DeleteOldUsers(maxAge time.Duration) error {
 	return h.parent.DeleteOldUsers(maxAge)
 }
 
-func HashUser(user string) string {
+func HashUserSha1(user string) string {
 	hash := sha1.New()
-	b := hash.Sum([]byte(user))
+	hash.Write([]byte(user))
+	b := hash.Sum(nil)
 	return fmt.Sprintf("%X", b)
+}
+
+type migrateToHashUser struct {
+	parent   FileSystemFactory
+	hashFunc HashFunc
+}
+
+func NewMigrateToHashUser(f FileSystemFactory) FileSystemFactory {
+	return migrateToHashUser{parent: f, hashFunc: HashUserSha1}
+}
+
+func (m migrateToHashUser) Create(user string, create bool) (fileSys.FileSystem, error) {
+	hash := m.hashFunc(user)
+	if create {
+		if m.parent.DoesUserExist(hash) {
+			return nil, os.ErrExist
+		} else {
+			if m.parent.DoesUserExist(user) {
+				log.Printf("create: found old user, try to rename")
+				err := m.parent.RenameUser(user, hash)
+				if err != nil {
+					log.Printf("could not rename user %s: %v", user, err)
+					return nil, err
+				}
+				log.Printf("renamed old user")
+				return nil, os.ErrExist
+			}
+			return m.parent.Create(hash, true)
+		}
+	} else {
+		if m.parent.DoesUserExist(hash) {
+			return m.parent.Create(hash, false)
+		} else {
+			if m.parent.DoesUserExist(user) {
+				log.Printf("open: found old user, try to rename")
+				err := m.parent.RenameUser(user, hash)
+				if err != nil {
+					log.Printf("could not rename user %s: %v", user, err)
+					return nil, err
+				}
+			}
+			log.Printf("renamed old user")
+			return m.parent.Create(hash, false)
+		}
+	}
+}
+
+func (m migrateToHashUser) DoesUserExist(user string) bool {
+	h := m.hashFunc(user)
+	if m.parent.DoesUserExist(h) {
+		return true
+	}
+	return m.parent.DoesUserExist(user)
+}
+
+func (m migrateToHashUser) DeleteOldUsers(maxAge time.Duration) error {
+	return m.parent.DeleteOldUsers(maxAge)
+}
+
+func (m migrateToHashUser) RenameUser(user string, newName string) error {
+	return m.parent.RenameUser(m.hashFunc(user), m.hashFunc(newName))
 }
